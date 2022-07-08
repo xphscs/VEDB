04207442728a00b0f321ad4e292cdb6d568002f5
Santiago Acosta <santiago.andres.acosta.diaz@gmail.com>
2022-07-07 20:41:39 -0500

    agregadas funciones con los widgets aparentemente necesarios

--- dependencies/TKu.py
+++ dependencies/TKu.py
@@ -3,7 +3,11 @@
 # --------------------------------------- #
 
 from tkinter import *
-from tkinter.ttk import Treeview
+from tkinter.ttk import Treeview, Combobox
+
+from numpy import var
+
+GBD = 2
 
 class Window:
 
@@ -25,7 +29,7 @@ class Window:
 
         # se crea una instancia del objeto button
         TB = Button(
-            self.ww, text = text, command = command, bg = bgc, bd = 2
+            self.ww, text = text, command = command, bg = bgc, bd = GBD
         )
 
         # se pocisiona el objeto
@@ -35,7 +39,7 @@ class Window:
     def CreateList(self, WWdim, relx, rely, columns, data):
 
         # se crea la instancia del objeto de lista
-        TV = Treeview(columns = columns[1::])
+        TV = Treeview(columns = columns[1::], bd = GBD)
 
         # se crean las columnas de los elementos
         TV.heading("#0", text = columns[0])
@@ -55,7 +59,65 @@ class Window:
             )
         
         TV.place(x = int(WWdim[0] * relx), y = int(WWdim[1] * rely))
+
+        return TV
+    
+    # función para display de mensajes en la ventana
+    def CreateLabel(self, WWdim, relx, rely, text):
+
+        LB = Label(self.ww, text = text, bd = GBD)
+
+        LB.place(x = int(WWdim[0] * relx), y = int(WWdim[1] * rely))
+
+    # función para crear espacios de texto
+    def CreateEntry(self, WWdim, relx, rely, width, height):
+
+        ET = Entry(self.ww)
+        ET.config(width = width, height = height)
+
+        ET.place(x = int(WWdim[0] * relx), y = int(WWdim[1] * rely))
+
+        return ET
+
+    # función que crea check buttons
+    def CreateCheckbutton(self, WWdim, relx, rely, variable):
+
+        CB = Checkbutton(self.ww, variable = variable)
+
+        CB.place(x = int(WWdim[0] * relx), y = int(WWdim[1] * rely))
+    
+    # función que crea menús en la barra superior de la ventana
+    def CreateMenu(self, items, submenus):
+
+        MN = Menu(self.ww, bd = GBD)
+
+        for (item, submenu) in zip(items, submenus):
+
+            MN.add_cascade(label = item, menu = submenu)
         
+        self.ww.config(menu = MN)
+
+        return MN
+    
+    # función para crear las cascadas de los menús
+    def CreateSubmenu(Mmenu, items, commands):
+
+        SBM = Menu(Mmenu, tearoff = 0)
+
+        for (item, command) in zip(items, commands):
+
+            SBM.add_command(label = item, command = command)
+        
+        return SBM
+
+    # función para crear una lista desplegable
+    def CreateCombobox(self, WWdim, relx, rely, values):
+
+        CB = Combobox(self.ww, state = "readonly", values = values)
+
+        CB.place(x = int(WWdim[0] * relx), y = int(WWdim[1] * rely))
+
+        return CB
 
     def FuncionPrueba(args):
 
